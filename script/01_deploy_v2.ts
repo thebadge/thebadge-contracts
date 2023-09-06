23c40e48994a00d900889ade1b66e01f4f3da14d
apane <agustin.pane@gmail.com>
2023-09-06 22:24:14 +0200

    Adds verification and deployment scripts

---
+++ script/01_deploy_v2.ts
@@ -0,0 +1,114 @@
+import * as dotenv from "dotenv";
+import { Contract } from "ethers";
+import hre, { upgrades } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Chains, contracts, isSupportedNetwork } from "./contracts";
+
+dotenv.config();
+
+async function main(hre: HardhatRuntimeEnvironment) {
+  const { network } = hre;
+
+  const chainId = network.config.chainId;
+  if (!chainId || !isSupportedNetwork(chainId)) {
+    throw new Error(`Network: ${chainId} is not defined or is not supported`);
+  }
+
+  // https://docs.openzeppelin.com/contracts/4.x/api/proxy#transparent_proxy
+  // https://docs.openzeppelin.com/learn/upgrading-smart-contracts#upgrading-a-contract-via-plugins
+
+  // Deploys the four main contracts: TheBadgeStore, TheBadgeUsers, TheBadgeModels, TheBadge (in that order)
+
+  console.log("Deploying Main contracts...");
+  const { mainContracts, theBadge, theBadgeStore } = await deployMainContracts(hre);
+
+  // Deploys all the controllers
+  const controllersAddresses = await deployControllers(hre, theBadge, theBadgeStore);
+
+  console.log("///////// Deployment finished /////////");
+  for (const mainContractsAddresses of mainContracts) {
+    console.log(`${mainContractsAddresses[0]}-${mainContractsAddresses[1]}`);
+  }
+  for (const controllerAddress of controllersAddresses) {
+    console.log(`${controllerAddress[0]}-${controllerAddress[1]}`);
+  }
+  console.log("///////// Deployment finished /////////");
+}
+
+const deployMainContracts = async (
+  hre: HardhatRuntimeEnvironment,
+): Promise<{ mainContracts: string[][]; theBadge: Contract; theBadgeStore: string }> => {
+  const { ethers } = hre;
+  const [deployer] = await ethers.getSigners();
+  const contractsAdmin = deployer.address;
+  const deployedAddresses = [];
+
+  console.log("Deploying TheBadgeStore...");
+  // const TheBadgeStore = await ethers.getContractFactory("TheBadgeStore");
+  // const theBadgeStore = await upgrades.deployProxy(TheBadgeStore, [contractsAdmin, contractsAdmin]);
+  // await theBadgeStore.deployed();
+  // console.log(`TheBadgeStore deployed with address: ${theBadgeStore.address}`);
+  // deployedAddresses.push(["TheBadgeStore", theBadgeStore.address]);
+  const theBadgeStoreAddress = "0x825280D57c28A65095E9998F1eC8D4E3Db87565f";
+
+  // console.log("Deploying TheBadgeUsers...");
+  // const TheBadgeUsers = await ethers.getContractFactory("TheBadgeUsersFacet");
+  // const theBadgeUsers = await upgrades.deployProxy(TheBadgeUsers, [contractsAdmin, theBadgeStoreAddress]);
+  // await theBadgeUsers.deployed();
+  // console.log(`TheBadgeUsers deployed with address: ${theBadgeUsers.address}`);
+  // deployedAddresses.push(["TheBadgeUsers", theBadgeUsers.address]);
+  //
+  // console.log("Deploying TheBadgeModels...");
+  // const TheBadgeModels = await ethers.getContractFactory("TheBadgeModelsFacet");
+  // const theBadgeModels = await upgrades.deployProxy(TheBadgeModels, [contractsAdmin, theBadgeStoreAddress]);
+  // await theBadgeModels.deployed();
+  // console.log(`TheBadgeModels deployed with address: ${theBadgeModels.address}`);
+  // deployedAddresses.push(["TheBadgeModels", theBadgeModels.address]);
+
+  console.log("Deploying TheBadge...");
+  const TheBadge = await ethers.getContractFactory("TheBadgeFacet");
+  const theBadge = await upgrades.deployProxy(TheBadge, [contractsAdmin, theBadgeStoreAddress]);
+  await theBadge.deployed();
+  console.log(`TheBadge deployed with address: ${theBadge.address}`);
+  deployedAddresses.push(["TheBadge", theBadge.address]);
+
+  return { mainContracts: deployedAddresses, theBadge, theBadgeStore: theBadgeStoreAddress };
+};
+
+const deployControllers = async (
+  hre: HardhatRuntimeEnvironment,
+  theBadge: Contract,
+  theBadgeStore: string,
+): Promise<string[][]> => {
+  const { ethers, network } = hre;
+  const [deployer] = await ethers.getSigners();
+  const chainId = network.config.chainId;
+  const lightGTCRFactory = contracts.LightGTCRFactory.address[chainId as Chains];
+  const klerosArbitror = contracts.KlerosArbitror.address[chainId as Chains];
+  console.log("Deploying KlerosBadgeModelController...");
+  // Deploys and adds all the controllers
+  const KlerosBadgeModelController = await ethers.getContractFactory("KlerosBadgeModelController");
+  // The admin that is allowed to upgrade the contracts
+  const contractsAdmin = deployer.address;
+  const klerosBadgeModelController = await upgrades.deployProxy(KlerosBadgeModelController, [
+    contractsAdmin,
+    theBadge.address,
+    klerosArbitror,
+    lightGTCRFactory,
+    theBadgeStore,
+  ]);
+  await klerosBadgeModelController.deployed();
+  console.log(`KlerosBadgeModelController deployed with address: ${klerosBadgeModelController.address}`);
+
+  console.log("Adding KlerosBadgeModelController to TheBadge...");
+  theBadge.connect(deployer);
+  await theBadge.addBadgeModelController("kleros", klerosBadgeModelController.address);
+  return [["klerosBadgeModelController", klerosBadgeModelController.address]];
+};
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main(hre).catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
