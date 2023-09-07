5dbe51eb85fd6e2da896a22fd427eb0a669da526
apane <agustin.pane@gmail.com>
2023-09-07 21:52:59 +0200

    Remove unused FEE_COLLECTOR_ROLE

--- src/contracts/thebadge/TheBadgeRoles.sol
+++ src/contracts/thebadge/TheBadgeRoles.sol
@@ -6,7 +6,6 @@ import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/ac
 contract TheBadgeRoles is AccessControlUpgradeable {
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant FEE_COLLECTOR_ROLE = keccak256("FEE_COLLECTOR_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant VERIFIER_ROLE = keccak256("VERIFIER_ROLE");
 }
