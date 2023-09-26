4391444196eb96149f83d2325346c411044c56d9
apane <agustin.pane@gmail.com>
2023-09-26 12:00:11 +0200

    Update tests

--- test/TheBadgeModels.t.sol
+++ test/TheBadgeModels.t.sol
@@ -15,10 +15,15 @@ contract TheBadgeModelsTest is Test {
     TheBadgeUsers public badgeUsersInstance;
     TheBadgeStore public badgeStoreInstance;
     KlerosBadgeModelController public klerosBadgeModelControllerInstance;
+    KlerosBadgeModelControllerStore public klerosBadgeModelControllerStoreInstance;
     address public admin = vm.addr(1);
     address public user1 = vm.addr(2);
     address public user2 = vm.addr(3);
     address public feeCollector = vm.addr(4);
+    // TCR Factory address in goerli
+    address public _tcrFactory = 0x55A3d9Bd99F286F1817CAFAAB124ddDDFCb0F314;
+    // Kleros arbitrator address in goerli
+    address public _arbitrator = 0x1128eD55ab2d796fa92D2F8E1f336d745354a77A;
     string public klerosControllerName = "Kleros";
 
     // Set up the contract instances before each test
@@ -47,14 +52,26 @@ contract TheBadgeModelsTest is Test {
         badgeModelsInstance.initialize(admin, address(badgeStoreInstance), address(badgeUsersInstance));
         console.log("badgeModelsInstance initialized with address: ", address(badgeModelsInstance));
 
+        // Instantiates the KlerosBadgeModelControllerStore
+        console.log("Initializing mockup klerosBadgeModelControllerStoreInstance...");
+        address klerosBadgeModelControllerStoreInstanceImp = address(new KlerosBadgeModelControllerStore());
+        address klerosBadgeModelControllerStoreProxy = ClonesUpgradeable.clone(
+            klerosBadgeModelControllerStoreInstanceImp
+        );
+        klerosBadgeModelControllerStoreInstance = KlerosBadgeModelControllerStore(
+            payable(klerosBadgeModelControllerStoreProxy)
+        );
+        klerosBadgeModelControllerStoreInstance.initialize(admin, _arbitrator, _tcrFactory);
+        console.log(
+            "klerosBadgeModelControllerStoreInstance initialized with address: ",
+            address(klerosBadgeModelControllerStoreInstance)
+        );
+
         // Instantiates the KlerosBadgeModelController
         console.log("Initializing mockup klerosBadgeModelControllerInstance...");
         // TODO: this _badgeContractAddress should be also a instance of TheBadge.sol contract and should be initialized the same way the rest of the contracts showed here
         address _badgeContractAddress = vm.addr(5);
-        address _arbitrator = vm.addr(6);
-        // Address of _tcrFactory in goerli, because we use a fork of goerli we have this already instanciated
-        // TODO: Maybe refactor this
-        address _tcrFactory = 0x55A3d9Bd99F286F1817CAFAAB124ddDDFCb0F314;
+
         address klerosBadgeModelInstanceImp = address(new KlerosBadgeModelController());
         address klerosBadgeModelControllerProxy = ClonesUpgradeable.clone(klerosBadgeModelInstanceImp);
         klerosBadgeModelControllerInstance = KlerosBadgeModelController(payable(klerosBadgeModelControllerProxy));
@@ -63,15 +80,14 @@ contract TheBadgeModelsTest is Test {
             _badgeContractAddress,
             address(badgeModelsInstance),
             address(badgeUsersInstance),
-            _arbitrator,
-            _tcrFactory
+            address(klerosBadgeModelControllerStoreInstance)
         );
         console.log(
             "klerosBadgeModelControllerInstance initialized with address: ",
             address(klerosBadgeModelControllerInstance)
         );
 
-        // Finally adds the permissions to TheBadgeModels and TheBadgeUsers to access the store...
+        // Adds the permissions to TheBadgeModels and TheBadgeUsers to access the store...
         console.log("Allowing TheBadgeUsers & TheBadgeModels to access the store...");
         vm.prank(admin);
         badgeStoreInstance.addPermittedContract("TheBadgeUsers", address(badgeUsersInstance));
@@ -91,6 +107,14 @@ contract TheBadgeModelsTest is Test {
         badgeStoreInstance.addBadgeModelController(klerosControllerName, klerosBadgeModelController);
         console.log("KlerosBadgeModelController has been added!");
 
+        // Finally adds the permission to KlerosBadgeModelController to access the KlerosBadgeModelControllerStore...
+        console.log("Allowing KlerosBadgeModelController to access the KlerosBadgeModelControllerStore...");
+        vm.prank(admin);
+        klerosBadgeModelControllerStoreInstance.addPermittedContract(
+            "KlerosBadgeModelController",
+            address(klerosBadgeModelControllerInstance)
+        );
+
         // Finally gives the role USER_MANAGER_ROLE to the contract TheBadgeModels to allow it to call the method makeUserCreator on the contract TheBadgeUsers
         // Fore more details you can check the 01_deploy.ts script inside the /script folder
         console.log("Granting USER_MANAGER_ROLE to the TheBadgeModels contract...");
@@ -100,7 +124,7 @@ contract TheBadgeModelsTest is Test {
         console.log("USER_MANAGER_ROLE granted!");
     }
 
-    function testCreateBadgeModel() public {
+    function testCreateKlerosBadgeModel() public {
         // Register a user
         vm.prank(user1);
         badgeUsersInstance.registerUser("ipfs://creatorMetadata.json", false);
@@ -118,7 +142,6 @@ contract TheBadgeModelsTest is Test {
         KlerosBadgeModelControllerStore.CreateBadgeModel memory klerosBadgeModel = KlerosBadgeModelControllerStore
             .CreateBadgeModel({
                 governor: vm.addr(1),
-                admin: admin,
                 courtId: 0,
                 numberOfJurors: 1,
                 registrationMetaEvidence: "ipfs://registrationMetaEvidence.json",
